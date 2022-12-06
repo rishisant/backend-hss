ccaaf3a0fcdf6659b795ffdbac35840c2b5f9703
rishi <65874016+rishisant@users.noreply.github.com>
2022-12-06 11:29:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const port = process.env.PORT || 3001
 const product_test = require('./test');
 
 app.use(express.json());
-// app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
 app.get('/', function(req, res) {
   res.send('Hello World!')
 });
