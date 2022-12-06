eb5710fcd6e1504519a94def2720482127a2a6ca
rishi <65874016+rishisant@users.noreply.github.com>
2022-12-06 11:35:45 -0600

    updating basic files
    ありがとう、ジョセフくん！

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3001
+const port = 3001
 
 const product_test = require('./test');
 
-app.use(express.json());
+app.use(express.json())
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-});
-
+app.get('/get_employees', (req, res) => {
+  product_test.getEmployees()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).send(error);
+  })
+})
 
 app.get('/products', (req, res) => {
   product_test.getProduct()
@@ -49,21 +55,97 @@ app.get('/get_orders', (req, res) => {
     res.status(500).send(error);
   })
 })
-// get emplyoees
-app.get('/get_employees', (req, res) => {
-  product_test.getEmployees()
+app.get('/get_inv', (req, res) => {
+  product_test.getInv()
   .then(response => {
     res.status(200).send(response);
   })
   .catch(error => {
     console.log(error);
-    res.status
-    (500).
-    send(error
-    );
+    res.status(500).send(error);
+  })
+})
+app.get('/get_menu', (req, res) => {
+  product_test.getMenu()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).send(error);
   })
 })
 
+
+app.delete('/delete_product', (req, res) => {
+  product_test.deleteProduct(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).send(error);
+  })
+})
+
+//update product
+app.put('/put_product', (req, res) => {
+  product_test.putProduct(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    console.log("put error");
+    res.status(500).send(error);
+  })
+})
+//add product
+app.post('/post_product', (req, res) => {
+  product_test.addProduct(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    console.log("post error");
+    res.status(500).send(error);
+  })
+})
+
+//post inv
+app.post('/post_inv', (req, res) => {
+  product_test.addInv(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    console.log("post error");
+    res.status(500).send(error);
+  })
+})
+
+//delete inv
+app.delete('/delete_inv', (req, res) => {
+  product_test.deleteInv(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    console.log("delete error");
+    res.status(500).send(error);
+  })
+})
+
+//put inv
+app.put('/put_inv', (req, res) => {
+  product_test.putInv(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    console.log("put error");
+    res.status(500).send(error);
+  })
+})
 
 app.post('/translate', (req, res) => {
   product_test.translateText(req, res)
--- test.js
+++ test.js
@@ -13,12 +13,44 @@ const pool = new Pool({
 const getOrders = async () => {
     //test addorder
     return new Promise(function(resolve, reject) {
-        pool.query('SELECT * FROM orders', (error, results) => {
+        pool.query('SELECT * FROM orders', async (error, results) => {
         if (error) {
             console.log("bad");
             reject(error)
         }
+        //console.log("here1");
         //console.log(results.rows[0]['product_id']);
+        //get the description of the product in each order and replace the product id with the description
+        pool.query('SELECT * FROM product', (error, results2) => {
+            if (error) {
+                console.log("bad");
+                reject(error)
+            }
+            //console.log("here");
+            //console.log(results.rows[0]['product_id']);
+            for (var i = 0; i < results.rows.length; i++){
+                var temp = results.rows[i]['product_ids'];  
+                var temp2 = [];
+                for (var j = 0; j < temp.length; j++){
+                    for (var k = 0; k < results2.rows.length; k++){
+                        if (temp[j] == results2.rows[k]['product_id']){
+                            temp2.push(results2.rows[k]['description']);
+                            //console.log(results2.rows[k]['description']);
+                        }
+                    }
+                }
+                results.rows[i]['product_ids'] = temp2;
+            }
+
+        })
+
+
+
+
+        //wait 2 seconds
+        await new Promise(r => setTimeout(r, 1500));
+
+
         resolve(results.rows);
         //console.log(results.rows[0]['description']);
         })
@@ -26,6 +58,8 @@ const getOrders = async () => {
         //console.log("here");
     }) 
 }
+
+
 const addOrder = async (order) => { 
     id_list = "{";
     quant_list = "{";
@@ -34,9 +68,18 @@ const addOrder = async (order) => {
         return new Promise((resolve, reject) => {
             for (var i = 0; i < order.length; i++){
                 pool.query("SELECT * from product where description='" + String(order[i].name) + "';", (error, get_id) => {
+                    //minus 1 from the inventory for each item in the prooduct
+                    pool.query("UPDATE ingredient SET quantity = quantity - 1 WHERE ing_id = ANY ('{" + String(get_id.rows[0]['ing_id']) + "}');", (error, results) => {
+                        if (error) {
+                            console.log("bad");
+                            reject(error)
+                        }
+                    })
+                    //console.log("UPDATE ingredient SET quantity = quantity - 1 WHERE ing_id = ANY ('{" + String(get_id.rows[0]['ing_id']) + "}');");
+
                     id_list += String(get_id.rows[0]['product_id']) + ",";
-                    console.log(get_id.rows[0]['product_id']);
-                    console.log("in loop", id_list);
+                    //console.log(get_id.rows[0]['product_id']);
+                    //console.log("in loop", id_list);
                 });
                 //add quantity to string list
                 quant_list += String(order[i].quantity) + ",";
@@ -76,7 +119,7 @@ const addOrder = async (order) => {
         return new Promise((resolve, reject) => {
             pool.query("SELECT MAX(id) from orders;", (error, get_id) => {
                 max_id = String(get_id.rows[0]['max']+1);
-                console.log(max_id);
+                //console.log(max_id);
             });
             resolve();
         });
@@ -85,11 +128,11 @@ const addOrder = async (order) => {
     await new Promise(resolve => setTimeout(resolve, 1500));
     //max_id = String(max_id + 1);
     total_price = String(total_price);
-    console.log(date_string);
-    console.log(id_list);
-    console.log(quant_list);
-    console.log(total_price);
-    console.log("max_id", max_id);
+    //console.log(date_string);
+    //console.log(id_list);
+    //console.log(quant_list);
+    //console.log(total_price);
+    //console.log("max_id", max_id);
         //     //"INSERT INTO orders(id, order_num, product_ids, in_progress, is_completed, total_price, date) VALUES(" + Main.order_id + ", " + Main.order_num + ", '" + id_list + "', FALSE, TRUE, " + total_price + ", '" + date + " " + time + "');"
 
     //make query string
@@ -105,26 +148,6 @@ const addOrder = async (order) => {
         });
         resolve();
     });
-
-
-    // return new Promise (function(resolve, reject){
-        
-    //     //"INSERT INTO orders(id, order_num, product_ids, in_progress, is_completed, total_price, date) VALUES(" + Main.order_id + ", " + Main.order_num + ", '" + id_list + "', FALSE, TRUE, " + total_price + ", '" + date + " " + time + "');"
-        
-    //     //remove last comma
-    //     //id_list = id_list.slice(0, -1);
-        
-        
-    //     pool.connect();
-    //     pool.query('SELECT * FROM product', (error, results) => {
-    //     if (error) {
-    //         console.log("bad");
-    //         reject(error)
-    //     }
-    //     resolve(results.rows);
-    //     console.log(results.rows[0]['description']);
-    //     })
-    // })
 }
 
 const getProduct = async () => {
@@ -143,35 +166,317 @@ const getProduct = async () => {
         //console.log("here");
     }) 
 }
-
-const getIngredient = () => {
+//get invetory query
+const getInv = async () => {
     return new Promise(function(resolve, reject) {
-        // console.log("getProduct");
-        const {Client, Pool} = require('pg');
-        const pool = new Pool({
-        user: 'csce315_903_juntunen',
-        host: 'csce-315-db.engr.tamu.edu',
-        database: 'csce315_903_13',
-        password: '630007600',
-        port: 5432,
-        ssl: {rejectUnauthorized: false}
-        });
-        pool.connect();
-        // const result = pool.query('SELECT * FROM product;')
-        // console.log(result);
-        // console.log("got Product");
-        pool.query('SELECT * FROM Ingredient', (error, results) => {
+        pool.query('SELECT * FROM ingredient order by ing_id', (error, results) => {
         if (error) {
             console.log("bad");
             reject(error)
         }
+        resolve(results.rows);
+        })
+
+    })
+}
+
+const getMenu = async () => {
+    //get the ingredients and swap the ingredient id with the ingredient name
+    ing_list = [];
+    
+
+    return new Promise(function(resolve, reject) {
+        pool.query('SELECT * FROM product order by product_id', async (error, results) => {
+            if (error) {
+                console.log("bad");
+                reject(error)
+            }
+            pool.query('SELECT * FROM ingredient order by ing_id', (error, result) => {
+                if (error) {
+                    console.log("bad");
+                    reject(error)
+                }
+                for (var i = 0; i < result.rows.length; i++){
+                    ing_list.push({i_id: result.rows[i]['ing_id'], name: result.rows[i]['name']});
+                }
+                //change the ingredient id to the ingredient name for each ingredient in each product
+                for (var i = 0; i < results.rows.length; i++){
+                    var ing = results.rows[i]['ing_id'];
+                    for (var j = 0; j < ing.length; j++){
+                        for (var k = 0; k < ing_list.length; k++){
+                            if (ing[j] == ing_list[k].i_id){
+                                ing[j] = ing_list[k].name;
+                            }
+                        }
+                    }
+                    results.rows[i]['ing_id'] = ing;
+                }
+            
+            })
+            await new Promise(resolve => setTimeout(resolve, 1500));
+            resolve(results.rows);
+        })
+    })
+}
+
+const getEmployees = async () => {
+    return new Promise(function(resolve, reject) {
+        pool.query('SELECT * FROM employee', (error, results) => {
+            if(error) {
+                console.log("getEmployees bad");
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const putProduct = async (product) => {
+    //update the product table at the id with the new values
+    console.log("put product");
+    console.log(product.name);
+    var name = product.name;
+    var price = product.price;
+    var ing = product.ing;
+    var id = product.product_id;
+    var cat = product.category;
+    //query ingredients table to get the ingredient id and convert the ingredient name to the id
+    var ing_ids = "";
+    var ing_list = [];
+    pool.query('SELECT * FROM ingredient order by ing_id', (error, result1) => {
+        if (error) {
+            console.log("bad");
+            reject(error)
+        }
+        for (var i = 0; i < result1.rows.length; i++){
+            ing_list.push({i_id: result1.rows[i]['ing_id'], name: result1.rows[i]['name']});
+        }
+        //change the ingredient id to the ingredient name for each ingredient in each product
+        for (var i = 0; i < ing.length; i++){
+            for (var j = 0; j < ing_list.length; j++){
+                if (ing[i] == ing_list[j].name){
+                    ing_ids += String(ing_list[j].i_id);
+                    if(i != ing.length - 1){
+                        ing_ids += ", ";
+                    }
+                }
+            }
+        }
+        //console.log(ing_ids);
         
         resolve(results.rows);
-        console.log(results.rows[0]['description']);
         })
-        console.log("here");
-    }) 
+
+    
 }
+
+// const getMenu = async () => {
+//     //get the ingredients and swap the ingredient id with the ingredient name
+//     ing_list = [];
+    
+
+//     return new Promise(function(resolve, reject) {
+//         pool.query('SELECT * FROM product order by product_id', async (error, results) => {
+//             if (error) {
+//                 console.log("bad");
+//                 reject(error)
+//             }
+//             pool.query('SELECT * FROM ingredient order by ing_id', (error, result) => {
+//                 if (error) {
+//                     console.log("bad");
+//                     reject(error)
+//                 }
+//                 for (var i = 0; i < result.rows.length; i++){
+//                     ing_list.push({i_id: result.rows[i]['ing_id'], name: result.rows[i]['name']});
+//                 }
+//                 //change the ingredient id to the ingredient name for each ingredient in each product
+//                 for (var i = 0; i < results.rows.length; i++){
+//                     var ing = results.rows[i]['ing_id'];
+//                     for (var j = 0; j < ing.length; j++){
+//                         for (var k = 0; k < ing_list.length; k++){
+//                             if (ing[j] == ing_list[k].i_id){
+//                                 ing[j] = ing_list[k].name;
+//                             }
+//                         }
+//                     }
+//                     results.rows[i]['ing_id'] = ing;
+//                 }
+            
+//             })
+//             await new Promise(resolve => setTimeout(resolve, 1500));
+//             resolve(results.rows);
+//         })
+//     })
+// }
+
+// const putProduct = async (product) => {
+//     //update the product table at the id with the new values
+//     console.log("put product");
+//     console.log(product.name);
+//     var name = product.name;
+//     var price = product.price;
+//     var ing = product.ing;
+//     var id = product.product_id;
+//     var cat = product.category;
+//     //query ingredients table to get the ingredient id and convert the ingredient name to the id
+//     var ing_ids = "";
+//     var ing_list = [];
+//     pool.query('SELECT * FROM ingredient order by ing_id', (error, result1) => {
+//         if (error) {
+//             console.log("bad");
+//             reject(error)
+//         }
+//         for (var i = 0; i < result1.rows.length; i++){
+//             ing_list.push({i_id: result1.rows[i]['ing_id'], name: result1.rows[i]['name']});
+//         }
+//         //change the ingredient id to the ingredient name for each ingredient in each product
+//         for (var i = 0; i < ing.length; i++){
+//             for (var j = 0; j < ing_list.length; j++){
+//                 if (ing[i] == ing_list[j].name){
+//                     ing_ids += String(ing_list[j].i_id);
+//                     if(i != ing.length - 1){
+//                         ing_ids += ", ";
+//                     }
+//                 }
+//             }
+//         }
+//         //console.log(ing_ids);
+        
+//     })
+
+//     await new Promise(resolve => setTimeout(resolve, 1500));
+//     return new Promise(function(resolve, reject) {
+//         pool.query("UPDATE product SET name = '" + name + "', price = " + price + ", cat_id = " + cat + ", ing_id = '{" + ing_ids + "}', description = '" + name + "' WHERE product_id = " + id + ";", (error, results) => {
+//             if (error) {
+//                 console.log("error editing product");
+//                 reject(error)
+//             }
+//             resolve("results.rows");
+//             //console.log("UPDATE product SET name = '" + name + "', price = " + price + ", ing_id = '{" + ing_ids + "}', description = '" + name + "' WHERE product_id = " + id + ";");
+
+//         })
+//     })
+// }
+
+//post inventory
+const addInv = async (ing) => {
+    //add the ingredient to the inventory
+    console.log("post inventory");
+    
+
+    return new Promise(function(resolve, reject) {
+        pool.query("INSERT INTO ingredient(ing_id, name, quantity) VALUES(" + ing.inv_id + ", '" + ing.name + "', " + ing.stock + ");", (error, results) => {
+            if (error) {
+                console.log("error adding ingredient");
+                reject(error)
+            }
+            resolve("results.rows");
+        })
+    })
+}
+
+const deleteInv = async (ing) => {
+    //delete the ingredient from the inventory
+    console.log("delete inventory");
+    console.log(ing);
+    return new Promise(function(resolve, reject) {
+        pool.query("DELETE FROM ingredient WHERE ing_id = " + ing.inv_id + ";", (error, results) => {
+            if (error) {
+                console.log("error deleting ingredient");
+                reject(error)
+            }
+            resolve("results.rows");
+        })
+    })
+}
+
+const putInv = async (ing) => {
+    //update the ingredient in the inventory
+    console.log("put inventory");
+
+    return new Promise(function(resolve, reject) {
+        pool.query("UPDATE ingredient SET name = '" + ing.name + "', quantity = " + ing.stock + " WHERE ing_id = " + ing.inv_id + ";", (error, results) => {
+            if (error) {
+                console.log("error editing ingredient");
+                reject(error)
+            }
+            resolve("results.rows");
+        })
+    })
+}
+
+
+
+
+const deleteProduct = async (id) => {
+    //delete the product at the id
+    //q: how do i access the id from the url
+    console.log(id);
+    console.log(id.value);
+    return new Promise(function(resolve, reject) {
+        pool.query("DELETE FROM product WHERE product_id = " + id.product_id + ";", (error, results) => {
+            if (error) {
+                console.log("DELETE FROM product WHERE product_id = " + id.product_id + ";");
+                reject(error)
+            }
+            resolve("deleted product");
+        })
+    })
+
+}
+
+//add product to database
+const addProduct = async (product) => {
+    // console.log("add product");
+    // console.log(product.name);
+    // console.log(product.price);
+    // console.log(product.ing);
+    // console.log(product.category);
+    // console.log(product.stock);
+
+    return new Promise(async function(resolve, reject) {
+        //query ingredients table to get the ingredient id and convert the ingredient name to the id
+        var ing_ids = "";
+        var ing_list = [];
+        pool.query('SELECT * FROM ingredient order by ing_id', (error, result1) => {
+            if (error) {
+                console.log("ingredient query bad");
+                reject(error)
+            }
+            for (var i = 0; i < result1.rows.length; i++){
+                ing_list.push({i_id: result1.rows[i]['ing_id'], name: result1.rows[i]['name']});
+            }
+            //console.log(ing_list);
+            //change the ingredient id to the ingredient name for each ingredient in each product
+            for (var i = 0; i < product.ing.length; i++){
+                for (var j = 0; j < ing_list.length; j++){
+                   // console.log(product.ing[i]);
+                    if (product.ing[i] == ing_list[j].name){
+                        //console.log("found ingredient");
+                        ing_ids += String(ing_list[j].i_id);
+                        if(i != product.ing.length - 1){
+                            ing_ids += ", ";
+                        }
+                    }
+                }
+            }
+            //console.log("id", ing_ids);
+
+        })
+        await new Promise(resolve => setTimeout(resolve, 1500));
+        pool.query("INSERT INTO product (product_id, name, description, stock, cat_id, price, ing_id) VALUES (" + product.product_id + ", '" + product.name + "', '" + product.name + "', 0, " + product.category + ", " + product.price + ", '{" + ing_ids + "}');", (error, results) => {
+            if (error) {
+                console.log("error adding product");
+                reject(error)
+            }
+            resolve("results.rows");
+        })
+    })
+}
+
+
+
+
 
 
 // app.listen(process.env.PSQL_PORT, () => {
@@ -197,15 +502,15 @@ const translateText = async (req, res, next) => {
     const text = req.body.text;
     const translate = new Translate({key: TRANSLATE_KEY});
 
-    console.log('target language is ' + targetLanguage);
-    console.log('text is ' + text);
+   //console.log('target language is ' + targetLanguage);
+    //console.log('text is ' + text);
 
     try {
         let [response] = await translate.translate(text, targetLanguage);
-        console.log('translation is successful');
+        //console.log('translation is successful');
         res.status(200).json(response);
     } catch (error) {
-        console.log('translation failed');
+        //console.log('translation failed');
         res.status(500).json(error);
     }
 }
@@ -214,6 +519,14 @@ module.exports = {
     getProduct,
     addOrder,
     getOrders,
-    getIngredient,
-    translateText
+    getInv,
+    getMenu,
+    putProduct,
+    deleteProduct,
+    addProduct,
+    addInv,
+    deleteInv,
+    putInv,
+    translateText,
+    getEmployees,
 }
